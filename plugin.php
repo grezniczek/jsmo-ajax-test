bde1936b781ecc9968f7da573791d5b2df8709f1
Günther Rezniczek <g.rezniczek@gmail.com>
2023-06-16 12:33:37 +0200

    Add functionality to plugin page

--- plugin.php
+++ plugin.php
@@ -2,7 +2,52 @@
 
     $img_src = $module->getUrl("img/1.png");
 
+    $module->initializeJavascriptModuleObject();
+    $jsmo_name = $module->getJavascriptModuleObjectName();
+
 ?>
 <h4>Plugin Test Page</h4>
 <p>The image source is: <b><?=htmlspecialchars($img_src)?></b></p>
 <img src="<?=$img_src?>" alt="1">
+<hr>
+<button class="btn btn-sm btn-primary" data-action="ajax">Make AJAX request</button>
+<pre data-output="data"></pre>
+<pre data-output="error"></pre>
+
+
+<script>
+    const JSMO = <?=$jsmo_name?>;
+    const $dataOut = $('[data-output="data"]');
+    const $errorOut = $('[data-output="error"]');
+    function clear() {
+        $dataOut.text('').hide();
+        $errorOut.text('').hide();
+    }
+    clear();
+    $('button[data-action="ajax"]').on('click', function() {
+        clear();
+        JSMO.ajax('test').then(function(response) {
+            $dataOut.text(JSON.stringify(response, null, 2)).show();
+        }).catch(function(err) {
+            $errorOut.text(err).show();
+            JSMO.log(err);
+        });
+    });
+
+
+</script>
+
+<style>
+    [data-output] {
+        max-width: 800px;
+        margin-top: 1em;
+    }
+    [data-output="data"] {
+        padding: 1em;
+        border: 1px solid green;
+    }
+    [data-output="error"] {
+        padding: 1em;
+        border: 1px solid red;
+    }
+</style>
